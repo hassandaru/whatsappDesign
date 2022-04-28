742e4162d467e46519234175e715ab09a028d9ae
hassandaru <hassandaru@gmail.com>
2022-04-28 18:09:33 +0930

    first commit

---
+++ whatsappDesign/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  whatsappDesign
+//
+//  Created by Hassan Sohail Dar on 23/4/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
